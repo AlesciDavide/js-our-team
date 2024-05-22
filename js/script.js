3ad95e76f5e7aae6dd178af9c2e126e11019f8fe
AlesciDavide <davi.alesci@email.com>
2024-05-22 14:24:19 +0200

    aggiunte le immagini al posto della stringa immagine

--- js/script.js
+++ js/script.js
@@ -4,37 +4,37 @@ const ourTeam = [
     {
         nome: 'Wayne Barnett',
         ruolo: 'Founder & CEO',
-        foto: 'wayne-barnett-founder-ceo.jpg',
+        foto: './img/wayne-barnett-founder-ceo.jpg',
     },
     {
         nome: 'Angela Caroll',
         ruolo: 'Chief Editor',
-        foto: 'angela-caroll-chief-editor.jpg',
+        foto: './img/angela-caroll-chief-editor.jpg',
     },
     {
         nome: 'Walter Gordon',
         ruolo: 'Office Manager',
-        foto: 'walter-gordon-office-manager.jpg',
+        foto: './img/walter-gordon-office-manager.jpg',
     },
     {
         nome: 'Angela Lopez',
         ruolo: 'Social Media Manager',
-        foto: 'angela-lopez-social-media-manager.jpg',
+        foto: './img/angela-lopez-social-media-manager.jpg',
     },
     {
         nome: 'Scott Estrada',
         ruolo: 'Developer',
-        foto: 'scott-estrada-developer.jpg',
+        foto: './img/scott-estrada-developer.jpg',
     },
     {
         nome: 'Barbara Ramos',
         ruolo: 'Graphic Designer',
-        foto: 'barbara-ramos-graphic-designer.jpg',
+        foto: './img/barbara-ramos-graphic-designer.jpg',
     }
 ]
 
 
-for(let i = 0; i < ourTeam.length; i++){
+/* for(let i = 0; i < ourTeam.length; i++){
     let singoloMembro = ourTeam[i];
     for (const key in ourTeam[i]) {
         console.log(singoloMembro[key]);
@@ -42,5 +42,19 @@ for(let i = 0; i < ourTeam.length; i++){
         pEl.append(singoloMembro[key]);
         sectionEl.appendChild(pEl);
     }
-}
+} */
 
+for(let i = 0; i < ourTeam.length; i++){
+    let singoloMembro = ourTeam[i];
+    for (const key in ourTeam[i]) {
+        if(key !== 'foto'){
+            let pEl = document.createElement('p');
+            pEl.append(singoloMembro[key]);
+            sectionEl.appendChild(pEl);
+        }else{
+            let imgEl = document.createElement('img');
+            imgEl.src = singoloMembro[key];
+            sectionEl.appendChild(imgEl);
+        }
+    }
+}
